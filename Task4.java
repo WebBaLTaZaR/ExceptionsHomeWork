f0ef31902a34e4d7c57eb4aafc367b4a96fe02e4
BaLTaZaR <gricaev_sergey@mail.ru>
2023-10-04 14:56:18 +0600

    решил четвёртую задачу

--- Task4.java
+++ Task4.java
@@ -1,5 +1,20 @@
 package exceptionsHomeWork;
+
+import java.util.Scanner;
+
 /*Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку. Пользователю должно показаться сообщение, что пустые строки вводить нельзя. */
 public class Task4 {
-	
+	public static void main(String[] args) {
+		try (Scanner in = new Scanner(System.in)) {
+			System.out.println("Введите что-нибудь: ");
+			String userIn = in.nextLine();
+
+			if (userIn.isEmpty()) {
+				throw new RuntimeException("пустые строки вводить нельзя");
+			} else {
+				System.out.println(userIn);
+			}
+		}
+	}
+
 }
