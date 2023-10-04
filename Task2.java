33b9c43799bd02a9c999d0b7c27fac408099107c
BaLTaZaR <gricaev_sergey@mail.ru>
2023-10-04 14:23:47 +0600

    решил вторую задачу

--- Task2.java
+++ Task2.java
@@ -1,5 +1,17 @@
 package exceptionsHomeWork;
 
+/*Если необходимо, исправьте данный код */
 public class Task2 {
-	
-}
+	public static void main(String[] args) {
+
+		try {
+			int d = 0;
+			int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+			double catchedRes1 = intArray[8] / d;
+			System.out.println("catchedRes1 = " + catchedRes1);
+		} catch (ArithmeticException e) {
+			System.out.println("Catching exception: " + e);
+		}
+	}
+
+}//TODO здесь была допущена ошибка в том, что переменная intArray не была инициализирована.
